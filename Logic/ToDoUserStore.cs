b7df34a93c05b35704a9522d95ae74a65ebd922a
Sondre Naustdal <sondre.naustdal@gmail.com>
2016-09-06 13:51:08 +0200

    Some cleanup

--- Logic/ToDoUserStore.cs
+++ Logic/ToDoUserStore.cs
@@ -112,29 +112,18 @@ namespace ToDoTnet.Logic
             {
                 if (disposing)
                 {
-                    // TODO: dispose managed state (managed objects).
+                    _ctx.SaveChanges();
                 }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
 
                 disposedValue = true;
             }
         }
 
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~UnaUserStore() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
-
         // This code added to correctly implement the disposable pattern.
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
             Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
+
         }
 
         public async Task SetPasswordHashAsync(TUser userIn, string passwordHash, CancellationToken cancellationToken)
